4671d08be28d50dd080cf2ac349991c8faa24621
Thiti-Dev <thiti.mwk.main@gmail.com>
2023-01-01 18:20:51 +0700

    DTO: create ScrapeBodyDto & ScrapeResponseBodyDTO

---
+++ Dtos/Scrape/Bodies/ScrapeBodyDto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace scraperor_v2.Dtos;
+
+public class ScrapeBodyPointerLookForDto
+{
+    [JsonPropertyName("tag")]
+    public string? Tag { get; set; }
+    [JsonPropertyName("has_classes")]
+    public string[]? HasClasses { get; set; }
+    [JsonPropertyName("then_look_for")]
+    public ScrapeBodyPointerLookForDto? ThenLookFor { get; set; }
+
+
+}
+
+public class ScrapeBodyPointerDto
+{
+    [Required(),JsonPropertyName("look_for")]
+    public ScrapeBodyPointerLookForDto? LookFor { get; set; }
+
+
+}
+
+public class ScrapeBodyDto
+	{
+    [Required(),JsonPropertyName("pointer")]
+    public ScrapeBodyPointerDto? Pointer { get; set; }
+    [Required(), JsonPropertyName("website")]
+    public string? Website { get; set; }
+
+}
+
---
+++ Dtos/Scrape/Responses/ScrapeResponseBodyDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace scraperor_v2.Dtos;
+
+public class ScrapeResponseBodyDto
+{
+
+    [JsonPropertyName("success")]
+    public bool? success { get; set; }
+    [JsonPropertyName("contents")]
+    public string[]? contents { get; set; }
+}
+

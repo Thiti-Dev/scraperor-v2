bd76a522bbe3a423137c5818e302e245173d7252
Thiti-Dev <thiti.mwk.main@gmail.com>
2023-01-01 18:22:45 +0700

    program: configured new dependency & configured the error formatter -> added SystemTextJsonValidationMetadataProvider to ModelMetaData

--- Program.cs
+++ Program.cs
@@ -1,11 +1,32 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
+using Microsoft.Extensions.DependencyInjection;
+using scraperor_v2.Services;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+/*builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.PropertyNamingPolicy = null;
+});*/
+
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient();
+builder.Services.AddScoped<IScrapeService, ScrapeService>();
+
+
+builder.Services.Configure<MvcOptions>(options =>
+{
+    options.ModelMetadataDetailsProviders.Add(
+        new SystemTextJsonValidationMetadataProvider());
+}); // format the error with the key according to the json exposed name
+
 
 var app = builder.Build();
 

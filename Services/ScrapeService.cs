54597952b758f5c79c0c5c60c084f47f92d6d041
Thiti-Dev <thiti.mwk.main@gmail.com>
2023-01-01 18:21:38 +0700

    implemented (Service-class)ScrapeService

---
+++ Services/ScrapeService.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using HtmlAgilityPack;
+using scraperor_v2.Dtos;
+
+namespace scraperor_v2.Services;
+
+public interface IScrapeService
+{
+    Task<string[]> ScrapeAsync(string url, ScrapeBodyPointerDto pointer);
+}
+
+public class ScrapeService : IScrapeService
+{
+    private readonly HttpClient _httpClient;
+
+    public ScrapeService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<string[]> ScrapeAsync(string url, ScrapeBodyPointerDto pointer)
+    {
+        var response = await _httpClient.GetAsync(url);
+        var content = await response.Content.ReadAsStringAsync();
+
+        HtmlDocument doc = new HtmlDocument();
+        doc.LoadHtml(content);
+
+
+
+        List<ScrapeBodyPointerLookForDto> steps = new List<ScrapeBodyPointerLookForDto>();
+
+
+        ScrapeBodyPointerLookForDto? currentLookFor = pointer.LookFor;
+        do
+        {
+            steps.Add(currentLookFor!);
+            currentLookFor = currentLookFor?.ThenLookFor;
+        }
+        while (currentLookFor != null);
+
+        dynamic virtualTraversal = doc.DocumentNode.AncestorsAndSelf();
+
+        int iter = 0;
+        foreach (var step in steps)
+        {
+            iter++;
+            Console.WriteLine(iter);
+            Console.WriteLine(step.Tag);
+            if (step.Tag != null)
+            {
+                virtualTraversal = ((IEnumerable<HtmlNode>)virtualTraversal).SelectMany(x => x.Descendants(step.Tag));
+            }
+
+            if (step.HasClasses != null && step.HasClasses.Length > 0)
+            {
+
+                foreach(string className in step.HasClasses)
+                {
+                    virtualTraversal = ((IEnumerable<HtmlNode>)virtualTraversal).SelectMany(x => x.AncestorsAndSelf().Where(z => z.HasClass(className)));
+                }
+            }
+        }
+
+
+        var virtualed = from v in virtualTraversal as IEnumerable<HtmlNode>
+                        select v.InnerText;
+
+
+        return virtualed.ToArray();
+    }
+}
+
